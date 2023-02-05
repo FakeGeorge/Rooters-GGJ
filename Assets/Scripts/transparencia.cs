cd48a5b5f4bdb9379b4e82b5db9b334d102c35b0
Alex <alexmaranon077@gmail.com>
2023-02-05 14:55:15 +0100

    fix transparency

--- Assets/Scripts/transparencia.cs
+++ Assets/Scripts/transparencia.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class transparencia : MonoBehaviour
 {
-    
-  
+
+
+    public List<GameObject> Objetos_dentro = new List<GameObject>();
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        var color = gameObject.GetComponent<Renderer>().material.color;
-        gameObject.GetComponent<Renderer>().material.color = new Color(color.r,color.g,color.b,0.5f);
+
+        Objetos_dentro.Add(collision.gameObject);
+
+        if (Objetos_dentro.Count >= 0)
+        {
+            var color = gameObject.GetComponent<Renderer>().material.color;
+            gameObject.GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b, 0.5f);
+        }
+
 
         Debug.Log("a");
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        var color = gameObject.GetComponent<Renderer>().material.color;
-        gameObject.GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b, 1f);
+        Objetos_dentro.Remove(collision.gameObject);
+
+        if (Objetos_dentro.Count == 0)
+        {
+            var color = gameObject.GetComponent<Renderer>().material.color;
+            gameObject.GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b, 1f);
+        }
+
     }
 }
